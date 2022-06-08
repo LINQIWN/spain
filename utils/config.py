661607b0016800b9a1287f9b9bfdd642d74efabf
LINQIWN <45078776+LINQIWN@users.noreply.github.com>
2022-06-08 23:10:11 +0100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ CHAT_ID = 5500232593 # which chat id to notify
 TIMEOUT = 60  # refresh page every 90s
 
 # =============== PERSONAL CONFIG FOR VISA ===============
-VISA_TYPE = 'Tourism'
+VISA_TYPE = 'Tourist'
 # YOU NEED TO CHANGE IT
 OPENED_PAGE = 'https://uk.blsspainvisa.com/visa4spain/book-date/X6SuqZKdjA' # your BLS appointment page link
 EMAIL = 'tsiven.lynn@yandex.ru' # BLS email
